48f45ebfd5e4601b1ce4ce44f795c70c6f6418a3
ByunSangJune <bsjjim@nate.com>
2023-11-29 22:52:27 +0900

    oop 코드 작성 집을 주제로 class를 만들어보기

--- src/main/java/com/example/demo/test/TemplateController.java
+++ src/main/java/com/example/demo/test/TemplateController.java
@@ -1,2 +1,30 @@
-package com.example.demo.test;public class TemplateController {
+package com.example.demo.test;
+
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.Parameter;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+@RequiredArgsConstructor
+public class TemplateController {
+    @Operation(summary = "회원 탈퇴 요청", description = "회원 정보가 삭제됩니다.", tags = { "Member Controller" })
+    @ApiResponses({
+            @ApiResponse(responseCode = "200", description = "OK", content = @Content(schema = @Schema(implementation = TemplateVO.class))),
+            @ApiResponse(responseCode = "400", description = "BAD REQUEST"),
+            @ApiResponse(responseCode = "404", description = "NOT FOUND"),
+            @ApiResponse(responseCode = "500", description = "INTERNAL SERVER ERROR")
+    })
+    @DeleteMapping("/v1/member/{id}")
+    public String deleteMember(@Parameter(description = "회원 ID", required = true, example = "1") @PathVariable("id") Long id) {
+        return new String();
+    }
 }
--- src/main/java/com/example/demo/test/TemplateVO.java
+++ src/main/java/com/example/demo/test/TemplateVO.java
@@ -1,2 +1,23 @@
-package com.example.demo.test;public class TemplateVO {
+package com.example.demo.test;
+
+import io.swagger.v3.oas.annotations.media.Schema;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.ToString;
+
+@Getter
+@Setter
+@ToString
+@NoArgsConstructor
+@Schema(description = "템플릿 관련 VO")
+public class TemplateVO {
+    @Schema(description = "템플릿 아이디")
+    private Integer templateId;
+
+    @Schema(description = "템플릿 이름")
+    private String templateName;
+
+    @Schema(description = "템플릿 기타")
+    private String templateEtc;
 }
